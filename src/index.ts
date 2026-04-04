0a7bc1c982252e9ad74096b2272441c8b8b7095b
GenaroMazur <genaromazur.business@gmail.com>
2026-04-04 20:12:53 -0300

    refactor: Extract database configuration to a dedicated datasource module and simplify core database initialization

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ import Server from "./infrastructure/Server";
 import indexRoute from "./infrastructure/http/index.route";
 import NotFoundController from "./infrastructure/http/NotFound.controller";
 import ErrorController from "./infrastructure/http/Error.controller";
+import Database from "./infrastructure/database/Database";
+import datasource from "./infrastructure/database/Datasource";
 
 const core = Core.instance
 
@@ -22,41 +24,9 @@ server.application.use(indexRoute)
 server.application.use(NotFoundController)
 server.application.use(ErrorController)
 
-import {UserEntity} from "./modules/users/infrastructure/sqlite/User.sqlite.entity";
-import Database from "./infrastructure/database/Database";
-import {DataSourceOptions} from "typeorm";
+core.database = new Database(datasource)
 
-const DB_HOST = process.env.DB_HOST
-const DB_PORT = process.env.DB_PORT
-const DB_USERNAME = process.env.DB_USERNAME
-const DB_PASSWORD = process.env.DB_PASSWORD
-
-if (!DB_HOST) throw new SystemException("DB_HOST is not defined in .env file")
-if (!DB_PORT) throw new SystemException("DB_PORT is not defined in .env file")
-if (!DB_USERNAME) throw new SystemException("DB_USERNAME is not defined in .env file")
-if (!DB_PASSWORD) throw new SystemException("DB_PASSWORD is not defined in .env file")
-
-const DB_TYPE = process.env.DB_TYPE || "sqlite"
-const dbOptions: DataSourceOptions = {
-    type: DB_TYPE as any,
-    database: process.env.DB_DATABASE || "database.sqlite",
-    synchronize: process.env.DB_SYNCHRONIZE === "true",
-    logging: process.env.DB_LOGGING === "true",
-    entities: [UserEntity],
-}
-if (DB_TYPE !== "sqlite") {
-    Object.assign(dbOptions, {
-        host: DB_HOST,
-        port: DB_PORT,
-        username: DB_USERNAME,
-        password: DB_PASSWORD,
-    })
-}
-
-core.database = new Database(dbOptions)
-
-core.start().then(() => {
-}).catch(CloseProcess(core))
+core.start().catch(CloseProcess(core))
 
 // graceful shutdown
 process.on('SIGINT', CloseProcess(core))
---
+++ src/infrastructure/database/Datasource.ts
@@ -0,0 +1,32 @@
+import {DataSourceOptions} from "typeorm";
+import {UserEntity} from "../../modules/users/infrastructure/sqlite/User.sqlite.entity";
+import SystemException from "../../share/exceptions/System.exception";
+
+const DB_TYPE = process.env.DB_TYPE || "sqlite"
+const dbOptions: DataSourceOptions = {
+    type: DB_TYPE as any,
+    database: process.env.DB_DATABASE || "database.sqlite",
+    synchronize: process.env.DB_SYNCHRONIZE === "true",
+    logging: process.env.DB_LOGGING === "true",
+    entities: [UserEntity],
+}
+if (DB_TYPE !== "sqlite") {
+    const DB_HOST = process.env.DB_HOST
+    const DB_PORT = process.env.DB_PORT
+    const DB_USERNAME = process.env.DB_USERNAME
+    const DB_PASSWORD = process.env.DB_PASSWORD
+
+    if (!DB_HOST) throw new SystemException("DB_HOST is not defined in .env file")
+    if (!DB_PORT) throw new SystemException("DB_PORT is not defined in .env file")
+    if (!DB_USERNAME) throw new SystemException("DB_USERNAME is not defined in .env file")
+    if (!DB_PASSWORD) throw new SystemException("DB_PASSWORD is not defined in .env file")
+
+    Object.assign(dbOptions, {
+        host: DB_HOST,
+        port: DB_PORT,
+        username: DB_USERNAME,
+        password: DB_PASSWORD,
+    })
+}
+
+export default dbOptions
